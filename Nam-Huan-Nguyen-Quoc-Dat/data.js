8cdbc0841d9ee9e6c0ce3de1b09ef21b74a35d7d
nauhnguyen <huannguyenhoang270903@gmail.com>
2024-03-13 19:47:42 +0700

    bai kiem tra giua ki

---
+++ Nam-Huan-Nguyen-Quoc-Dat/data.js
@@ -0,0 +1,37 @@
+var restaurant = [
+  {
+      id: 1,
+      name: "Nén Restaurant",
+      address: "123 Hải Phòng",
+      price: 500,
+      image: "https://tourdanangcity.vn/wp-content/uploads/2022/11/nha-hang-o-da-nang.jpg?v=1671784822"
+  },
+  {
+      id: 2,
+      name: "Deck Saigon",
+      address: "76 Tân Bình",
+      price: 300,
+      image: "https://tourdanangcity.vn/wp-content/uploads/2023/04/mon-an-nha-hang-bep-xua.png?v=1682437040"
+  },
+  {
+      id: 3,
+      name: "Thái Market",
+      address: "12 Lê Hồng Phong",
+      price: 200,
+      image: "https://tourdanangcity.vn/wp-content/uploads/2023/04/khong-gian-nha-hang.png?v=1682435660"
+  },
+  {
+      id: 4,
+      name: "Golden",
+      address: "28 Nguyễn Du",
+      price: 800,
+      image: "https://tourdanangcity.vn/wp-content/uploads/2023/05/nha-hang-da-nang-sang-trong.png?v=1685465835"
+  },
+  {
+      id: 5,
+      name: "Li Bai",
+      address: "88 Đồng Khời",
+      price: 400,
+      image: "https://tourdanangcity.vn/wp-content/uploads/2020/09/nha-hang-han-quoc.jpg?v=1668833968"
+  }
+];
